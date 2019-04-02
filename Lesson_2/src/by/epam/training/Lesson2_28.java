3761bcd900019d900a2a44fef241e26f3f11554d
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:34:04 +0300

    Update Lesson2_28.java

--- Lesson_2/src/by/epam/training/Lesson2_28.java
+++ Lesson_2/src/by/epam/training/Lesson2_28.java
@@ -5,20 +5,20 @@ public class Lesson2_28 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 28
-		double r, g, m, s;
-		r = 0.5; //радиан
 		
-		// 1 радиан = 180 гр / пи
+		double r, g, m, s;
+		r = 0.5; //Г°Г Г¤ГЁГ Г­
+		
+		// 1 СЂР°РґРёР°РЅ = 180 ГЈГ° / РџРё
 		g = r * 180 / Math.PI;
 		System.out.println(g);
 		int gr = (int)g;
-		//1 градус - 60 минут
+		//1 РіСЂР°РґСѓСЃ - 60 РјРёРЅСѓС‚
 		double temp1 = g - gr;
 		m = temp1 * 60;
 		System.out.println(m);
 		int min = (int)m;
-		// 1 минута - 60 секунд
+		// 1 РјРёРЅСѓС‚Р° - 60 СЃРµРєСѓРЅРґ
 		double temp2 = m - min;
 		s = temp2 * 60;
 		int sec = (int)s;
