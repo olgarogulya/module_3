a34ee766b9736efd80dcc1bc5333f1e1532c9038
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:27:35 +0300

    Update Lesson2_20.java

--- Lesson_2/src/by/epam/training/Lesson2_20.java
+++ Lesson_2/src/by/epam/training/Lesson2_20.java
@@ -5,17 +5,17 @@ public class Lesson2_20 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//задание 20
-		double l; //длина окружности
-		double r; //радиус
-		double s; //площадь
+		
+		double l; 
+		double r; 
+		double s; 
 		
 		l = 30.5;
 		
 		r = l / (2 * Math.PI);
 		s = Math.PI * r * r;
 		
-		System.out.println("площадь круга " + s);
+		System.out.println("РїР»РѕС‰Р°РґСЊ РєСЂСѓРіР° " + s);
 	}
 
 }
