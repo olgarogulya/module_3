ce23b8baa5864ee50ee55696c7e8f52c8aad94ba
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:31:23 +0300

    Update Lesson2_24.java

--- Lesson_2/src/by/epam/training/Lesson2_24.java
+++ Lesson_2/src/by/epam/training/Lesson2_24.java
@@ -5,16 +5,16 @@ public class Lesson2_24 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 24
-		double a, b, y, S; //сторона a, сторона b, угол и площадь
-		double h; //высота трапеции
+		
+		double a, b, y, S; 
+		double h; 
 		a = 10.0;
 		b = 6.0;
-		y = 60.0; //угол в градусах
+		y = 60.0; //СѓРіРѕР» РІ РіСЂР°РґСѓСЃР°С…
 		
 		h = (a - b) / 2 * Math.tan(y * Math.PI / 180);
 		S = (a + b) / 2 * h;
-		System.out.println("площадь " + S);
+		System.out.println("РїР»РѕС‰Р°РґСЊ " + S);
 	}
 
 }
