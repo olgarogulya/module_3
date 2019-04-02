8bb6e9f42c13ea736db22df637c6874c4abcbecb
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:25:25 +0300

    Update Lesson2_18.java

--- Lesson_2/src/by/epam/training/Lesson2_18.java
+++ Lesson_2/src/by/epam/training/Lesson2_18.java
@@ -5,17 +5,17 @@ public class Lesson2_18 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 18
-		double a; //длина грани куба
-		double S, Sk, V; //площадь грани, площадь полной поверхности, объем куба
+		
+		double a; 
+		double S, Sk, V; 
 		
 		a = 3.0;
 		S = a * a;
 		Sk = S * 6;
 		V = a * a * a;
-		System.out.println("площадь грани " + S);
-		System.out.println("площадь всей поверхности " + Sk);
-		System.out.println("объем куба " + V);
+		System.out.println("РїР»РѕС‰Р°РґСЊ РіСЂР°РЅРё " + S);
+		System.out.println("РїР»РѕС‰Р°РґСЊ РєСѓР±Р° " + Sk);
+		System.out.println("РѕР±СЉРµРј " + V);
 	}
 
 }
