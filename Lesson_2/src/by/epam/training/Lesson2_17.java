363c11019588de7470dcf31750819d2def58671a
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:24:29 +0300

    Update Lesson2_17.java

--- Lesson_2/src/by/epam/training/Lesson2_17.java
+++ Lesson_2/src/by/epam/training/Lesson2_17.java
@@ -5,19 +5,19 @@ public class Lesson2_17 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 17
+		
 		double a,b;
-		double c; //среднее арифметическое
-		double d; //среднее геометрическое
+		double c; 
+		double d; 
 		a = 21.0;
 		b = -12.0;
 		
 		c = (a + b) / 2;
-		System.out.println("среднее арифметическое " + c);
+		System.out.println("СЃСЂРµРґРЅРµРµ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРѕРµ " + c);
 		
 		d = Math.sqrt(Math.abs(a) * Math.abs(b));
 		//d = Math.pow((Math.abs(a) * Math.abs(b)), (1 / 2.0));
-		System.out.println("среднее геометрическое " + d);
+		System.out.println("СЃСЂРµРґРЅРµРµ РіРµРѕРјРµС‚СЂРёС‡РµСЃРєРѕРµ " + d);
 	}
 
 }
