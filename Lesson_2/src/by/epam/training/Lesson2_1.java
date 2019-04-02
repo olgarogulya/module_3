a65a05ff583ef77e4bc75d9c8b7a676893cb8036
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:20:49 +0300

    Update Lesson2_1.java

--- Lesson_2/src/by/epam/training/Lesson2_1.java
+++ Lesson_2/src/by/epam/training/Lesson2_1.java
@@ -3,15 +3,14 @@ package by.epam.training;
 public class Lesson2_1 {
 	public static void main(String[] args) {
 		
-		//task 1
+		
 		int x, y;
 		x = 3;
 		y = 2;
 				
 		System.out.println("sum " + x + y);
-		//System.out.println("dif " + x - y);//error undefined
 		System.out.println(x - y);
-		System.out.println("mulipl " + x * y);
+		System.out.println("multipl " + x * y);
 		System.out.println("division " + x / y);
 		
 	}
