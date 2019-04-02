6f866df3136309a5402be7325bf16957ccb2656f
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:40:33 +0300

    Update Lesson2_35.java

--- Lesson_2/src/by/epam/training/Lesson2_35.java
+++ Lesson_2/src/by/epam/training/Lesson2_35.java
@@ -5,22 +5,22 @@ public class Lesson2_35 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 35
+		
 		double M, N;
 		M = 12151.7;
 		N = 4.2;
 		
 		double a = M / N;
-		System.out.println("число " + a);
+		System.out.println("С‡РёСЃР»Рѕ " + a);
 		int a1 = (int)a;
 		//System.out.println("a1=" + a1);
 		int d = a1 % 10;
-		System.out.println("младшая цифра целой части " + d);
+		System.out.println("РјР»Р°РґС€Р°СЏ С†РёС„СЂР° С†РµР»РѕР№ С‡Р°СЃС‚Рё " + d);
 		
 		double a2 = (a - a1) * 10;
 		//System.out.println("a2=" + a2);
 		int e = (int)a2;
-		System.out.println("старшая цифра дробной части " + e);
+		System.out.println("СЃС‚Р°СЂС€Р°СЏ С†РёС„СЂР° С†РµР»РѕР№ С‡Р°СЃС‚Рё " + e);
 	
 	}	
 	}
