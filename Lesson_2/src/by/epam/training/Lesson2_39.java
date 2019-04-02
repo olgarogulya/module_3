16c24964469973e2e58fb8375f9ec1bd32925266
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:46:17 +0300

    Update Lesson2_39.java

--- Lesson_2/src/by/epam/training/Lesson2_39.java
+++ Lesson_2/src/by/epam/training/Lesson2_39.java
@@ -7,11 +7,11 @@ public class Lesson2_39 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//задание 39
+		
 		
 		double x = 2.0;
 		double a, b;
-		a = 2*x*x*x*x-3*x*x*x+4*x*x-5*x+6; //14 операторов
+		a = 2*x*x*x*x-3*x*x*x+4*x*x-5*x+6; //14 
 		System.out.println(a);
 		
 		b = x*(x*x*(2*x-3)+4*x-5)+6; //9
