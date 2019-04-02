50eee795a27c722d4b55dee057161d58165c0aca
olgarogulya <49169103+olgarogulya@users.noreply.github.com>
2019-04-02 13:36:19 +0300

    Update Lesson2_31.java

--- Lesson_2/src/by/epam/training/Lesson2_31.java
+++ Lesson_2/src/by/epam/training/Lesson2_31.java
@@ -5,7 +5,7 @@ public class Lesson2_31 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//задание 31
+		
 		double v, v1, t1, t2, l;
 		v = 5.0;
 		v1 = 1.4;
@@ -13,7 +13,7 @@ public class Lesson2_31 {
 		t2 = 1.5;
 		
 		l = v*t1 + (v-v1)*t2;
-		System.out.println("расстояние " + l);
+		System.out.println("СЂР°СЃСЃС‚РѕСЏРЅРёРµ " + l);
 
 	}
 
